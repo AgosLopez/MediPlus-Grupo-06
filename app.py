616e81b252d4c7ffbc8a01d871c9eb209accec00
Micaela Cembal <micaelacembal@gmail.com>
2026-05-24 13:24:06 -0300

    Conexión a Cassandra

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from dotenv import load_dotenv
 import os
 import redis
+from astrapy import DataAPIClient
 
 load_dotenv()
+
+# ==========================================
+# REDIS CLOUD
+# ==========================================
 
 r = redis.Redis(
     host=os.getenv("REDIS_HOST"),
@@ -20,4 +25,25 @@ r.set("prepaga", "MediPlus")
 # obtener dato
 valor = r.get("prepaga")
 
-print("Valor:", valor)
+print("Valor Redis:", valor)
+
+
+# ASTRA CON CASSANDRA
+
+
+client = DataAPIClient()
+
+db = client.get_database(
+    os.getenv("ASTRA_DB_API_ENDPOINT"),
+    token=os.getenv("ASTRA_DB_APPLICATION_TOKEN")
+)
+
+print("Conectado a Cassandra Astra")
+
+# ver collections
+collections = db.list_collection_names()
+
+print("Collections:")
+
+for c in collections:
+    print("-", c)
